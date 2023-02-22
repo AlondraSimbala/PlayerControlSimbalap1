8bb96410fd17a0f17f1faa8512fd5efc93879284
AlondraSimbala <alondrasimbala@gmail.com>
2023-02-22 09:55:08 -0800

    Finished PlayerControl
    Added vehicles to move forward on their own so the player can avoid them.
    Added a second camera, when pressing F you may switch between the two cameras.

--- PlayerControlSimbala/Assets/Scripts/PlayerControl.cs
+++ PlayerControlSimbala/Assets/Scripts/PlayerControl.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+    private float turnSpeed = 60.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +27,11 @@ public class PlayerControl : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Rotates Car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
